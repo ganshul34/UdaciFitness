fc8d52e90d43557ec9703f097491495c49eaf9b6
Anshul Gupta <anshul1144.cse16@chitkara.edu.in>
2018-08-18 02:34:15 +0530

    Changes in code

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -37,25 +37,27 @@ export default class AddEntry extends Component {
         })   
    }
    slide = ( metric,value ) =>{
-       this.setState(() => {
-         [metric] :value  
-       })
-       
-   }
-    render() {
+       this.setState(() =>({
+                [metric]: value
+
+       }))
+       }
+   
+       render() {
            
            const metaInfo = getMetricMetaInfo()
         return(
         <View>
-           {Object.keys(metaInfo).map((key) =>
+           {  Object.keys(metaInfo).map((key) =>{
            const {getIcon, type, ...rest } = metaInfo[key]
             const value = this.state[key]
-            )}
+          
        return(
        <View key={key}>
-           {getIcon()}
+           { getIcon() }
            </View>
        )
+    })}
             </View>
         )
     }
